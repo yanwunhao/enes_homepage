d334ca767ebe1c575e189db12f47cc6b0de8ba8c
rubieywh <rubieywh@aliyun.com>
2021-01-05 11:49:28 +0900

    init homepage

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import './public.css'
+import './reset.css'
+
+import Logo from './asset/muit_logo.png'
+
+import * as pb from './util/pagebuilder'
+
+// Set up header
+const header = pb.header_factory()
+
+const title = pb.paragraph_factory('Welcome to Emerging Networks and Systems Laboratory (ENeS)', 'title')
+
+const logo = pb.image_factory_by_id(Logo, 'muit_logo')
+
+header.appendChild(logo)
+
+header.appendChild(title)
+
+document.body.appendChild(header)
